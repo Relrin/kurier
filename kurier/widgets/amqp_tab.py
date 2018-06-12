3f1910fb282ef34465992cd2aacabf335dcd4947
Valeryi Savich <relrin78@gmail.com>
2018-06-12 12:38:14 +0300

    Implemented AMQP Tab

---
+++ kurier/widgets/amqp_tab.py
@@ -0,0 +1,10 @@
+import wx
+
+
+class AmqpTab(wx.Panel):
+    def __init__(self, parent, *args, **kwargs):
+        super(AmqpTab, self).__init__(parent, *args, **kwargs)
+        self.InitUI()
+
+    def InitUI(self):
+        pass
--- kurier/widgets/work_area.py
+++ kurier/widgets/work_area.py
@@ -1,14 +1,26 @@
 import wx
+from wx.aui import AuiNotebook
 
-from kurier.constants import DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP
+from kurier.constants import DEFAULT_GAP, DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP
+from kurier.widgets.amqp_tab import AmqpTab
 
 
 class WorkAreaPanel(wx.Panel):
 
     def __init__(self, parent, *args, **kwargs):
         super(WorkAreaPanel, self).__init__(parent, *args, **kwargs)
-        self.grid = wx.GridBagSizer(DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP)
+        self.notebook = AuiNotebook(self)
+        self.sizer = wx.GridBagSizer(DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP)
         self.InitUI()
 
     def InitUI(self):
-        self.SetSizer(self.grid)
+        self.AddNewTab()
+        self.sizer.Add(self.notebook, pos=(0, 0), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
+
+        self.sizer.AddGrowableCol(0)
+        self.sizer.AddGrowableRow(0)
+        self.SetSizer(self.sizer)
+
+    def AddNewTab(self, tab_name="New tab"):
+        new_tab = AmqpTab(self.notebook)
+        self.notebook.AddPage(new_tab, tab_name, select=True)
