f900e1dff9bdd59e2678f0338f26fc3ca7be9f40
Valeryi Savich <relrin78@gmail.com>
2018-06-15 21:36:00 +0300

    Configured resizable row

--- kurier/widgets/amqp_tab.py
+++ kurier/widgets/amqp_tab.py
@@ -1,10 +1,21 @@
 import wx
 
+from kurier.constants import DEFAULT_GAP, DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP
+from kurier.widgets.request.ui import RequestUIBlock
+
 
 class AmqpTab(wx.Panel):
     def __init__(self, parent, *args, **kwargs):
         super(AmqpTab, self).__init__(parent, *args, **kwargs)
+        self.grid = wx.GridBagSizer(DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP)
+        self.request_ui_block = None
+
         self.InitUI()
 
     def InitUI(self):
-        pass
+        self.request_ui_block = RequestUIBlock(self)
+        self.grid.Add(self.request_ui_block, pos=(0, 0), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
+
+        self.grid.AddGrowableCol(0)
+        self.grid.AddGrowableRow(0)
+        self.SetSizer(self.grid)
