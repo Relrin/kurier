36aa6a10e80668c8ac5a797d11b1f3cc7a6553fd
Valeryi Savich <relrin78@gmail.com>
2018-06-19 17:16:22 +0300

    Implemented EditableListCtrl class

--- kurier/widgets/resizable_list_ctrl.py
+++ kurier/widgets/resizable_list_ctrl.py
@@ -1,6 +1,8 @@
+from bisect import bisect
+
 import wx
 
-from wx.lib.mixins.listctrl import ListCtrlAutoWidthMixin
+from wx.lib.mixins.listctrl import ListCtrlAutoWidthMixin, TextEditMixin
 
 
 class ResizableListCtrl(ListCtrlAutoWidthMixin, wx.ListCtrl):
@@ -10,6 +12,7 @@ class ResizableListCtrl(ListCtrlAutoWidthMixin, wx.ListCtrl):
         | wx.LC_SINGLE_SEL
     DEFAULT_UTILITY_ROW_TEXT = "Click here for adding a new row..."
     DEFAULT_COLUMNS = ["Key", "Value"]
+    has_utility_row = False
 
     def __init__(self, *args, **kwargs):
         self.columns = kwargs.pop("columns", self.DEFAULT_COLUMNS)
@@ -29,12 +32,37 @@ class ResizableListCtrl(ListCtrlAutoWidthMixin, wx.ListCtrl):
         total_rows = self.GetItemCount()
         insert_index = total_rows if total_rows > 0 else 0
         self.InsertItem(insert_index, label=self.default_utility_row_text)
+        self.has_utility_row = True
 
     def DeleteUtilityRow(self):
         last_item_index = self.GetItemCount() - 1
         self.DeleteItem(last_item_index)
+        self.has_utility_row = False
 
     def AddNewRow(self, key, value=wx.EmptyString, index=None):
         insert_index = index if index is not None else self.GetItemCount()
         self.InsertItem(insert_index, key)
         self.SetItem(insert_index, 1, value)
+
+
+class EditableListCtrl(TextEditMixin, ResizableListCtrl):
+
+    def __init__(self, *args, **kwargs):
+        ResizableListCtrl.__init__(self, *args, **kwargs)
+        TextEditMixin.__init__(self)
+
+    def OnLeftDown(self, event=None):
+        x, y = event.GetPosition()
+
+        loc = 0
+        self.col_locs = [0]
+        for n in range(self.GetColumnCount()):
+            loc = loc + self.GetColumnWidth(n)
+            self.col_locs.append(loc)
+
+        col = bisect(self.col_locs, x + self.GetScrollPos(wx.HORIZONTAL)) - 1
+        if self.has_utility_row and col == self.GetItemCount() - 1:
+            event.Skip()
+            return
+
+        super(EditableListCtrl, self).OnLeftDown(event)
