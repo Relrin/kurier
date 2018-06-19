6c136051ac426895541e0883f9c45a3176aa183a
Valeryi Savich <relrin78@gmail.com>
2018-06-19 16:53:33 +0300

    Added functionality for displaying message response properties

--- kurier/widgets/response/properties.py
+++ kurier/widgets/response/properties.py
@@ -1,12 +1,27 @@
 import wx
 
+from kurier.widgets.resizable_list_ctrl import ResizableListCtrl
+
 
 class ResponsePropertiesTab(wx.Panel):
+    USED_COLUMNS = ["Property name", "Value"]
 
     def __init__(self, *args, **kwargs):
         super(ResponsePropertiesTab, self).__init__(*args, **kwargs)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.properties_ctrl = ResizableListCtrl(self, columns=self.USED_COLUMNS)
 
         self.InitUI()
 
     def InitUI(self):
-        pass
+        self.sizer.Add(self.properties_ctrl, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(self.sizer)
+
+    def AddRows(self, iterable):
+        for key, value in iterable:
+            total_rows = self.properties_ctrl.GetItemCount()
+            index = self.properties_ctrl.InsertItem(total_rows, label=key)
+            self.properties_ctrl.SetItem(index, 1, value)
+
+    def DeleteAllRows(self):
+        self.properties_ctrl.DeleteAllItems()
