028ae66d8517d6c5dc4ab12663e370b61f8bae87
Valeryi Savich <relrin78@gmail.com>
2018-06-20 11:09:52 +0300

    Added functionality for displaying response headers

--- kurier/widgets/response/headers.py
+++ kurier/widgets/response/headers.py
@@ -1,12 +1,25 @@
 import wx
 
+from kurier.widgets.list_ctrl import ResizableListCtrl
+
 
 class ResponseHeadersTab(wx.Panel):
+    USED_COLUMNS = ["Header name", "Value"]
 
     def __init__(self, *args, **kwargs):
         super(ResponseHeadersTab, self).__init__(*args, **kwargs)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.headers_ctrl = ResizableListCtrl(self, columns=self.USED_COLUMNS)
 
         self.InitUI()
 
     def InitUI(self):
-        pass
+        self.sizer.Add(self.headers_ctrl, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(self.sizer)
+
+    def AddRows(self, iterable):
+        for key, value in iterable:
+            self.headers_ctrl.AddNewRow(key, value)
+
+    def DeleteAllRows(self):
+        self.headers_ctrl.DeleteAllItems()
