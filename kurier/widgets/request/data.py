7205f54d83dc49d58d7b02d692b7bc9393cd6bdf
Valeryi Savich <relrin78@gmail.com>
2018-06-20 17:41:53 +0300

    Added TextCtrl for adding a message body for request

--- kurier/widgets/request/data.py
+++ kurier/widgets/request/data.py
@@ -1,12 +1,17 @@
 import wx
 
+from kurier.widgets.text_ctrl import TextCtrl
+
 
 class RequestDataTab(wx.Panel):
 
     def __init__(self, *args, **kwargs):
         super(RequestDataTab, self).__init__(*args, **kwargs)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.message_body_ctrl = TextCtrl(self)
 
         self.InitUI()
 
     def InitUI(self):
-        pass
+        self.sizer.Add(self.message_body_ctrl, proportion=1, flag=wx.EXPAND)
+        self.SetSizer(self.sizer)
