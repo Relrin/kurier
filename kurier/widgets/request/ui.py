2bc60f4bd56460afd1995d2b2865fc6a5b8c8905
Valeryi Savich <relrin78@gmail.com>
2018-06-22 10:52:43 +0300

    Update widgets location in request UI block

--- kurier/widgets/request/ui.py
+++ kurier/widgets/request/ui.py
@@ -26,40 +26,70 @@ class RequestUIBlock(ScrolledPanel):
 
     def InitUI(self):
         self.connection_string = wx.TextCtrl(self)
-        self.connection_string.SetHint("Connection string in `schema://username:password@host:port/vhost/?query` format")
+        self.connection_string.SetHint("schema://username:password@host:port/vhost/?query")
         self.grid.Add(
             self.connection_string,
             pos=(0, 0),
-            span=(0, 4),
+            span=(0, 3),
             flag=wx.EXPAND | wx.ALL,
             border=DEFAULT_GAP
         )
-
-        self.request_exchange_name_input = wx.TextCtrl(self)
-        self.request_exchange_name_input.SetHint("Request exchange name")
-        self.grid.Add(self.request_exchange_name_input, pos=(1, 0), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
-
-        self.request_routing_key_input = wx.TextCtrl(self)
-        self.request_routing_key_input.SetHint("Request routing key")
-        self.grid.Add(self.request_routing_key_input, pos=(1, 1), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
-
-        self.response_queue_name_input = wx.TextCtrl(self)
-        self.response_queue_name_input.SetHint("Response queue name")
-        self.grid.Add(self.response_queue_name_input, pos=(2, 0), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
-
-        self.response_exchange_name_input = wx.TextCtrl(self)
-        self.response_exchange_name_input.SetHint("Response exchange name")
-        self.grid.Add(self.response_exchange_name_input, pos=(2, 1), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
-
-        self.response_exchange_name_input = wx.TextCtrl(self)
-        self.response_exchange_name_input.SetHint("Response routing key")
-        self.grid.Add(self.response_exchange_name_input, pos=(2, 2), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
 
         self.send_button = wx.Button(self, label="Send", style=wx.BORDER_NONE)
         self.send_button.SetBackgroundColour("#20A5FF")
         self.send_button.SetForegroundColour('#FFFFFF')
         self.send_button.SetFont(wx.Font(wx.FontInfo(self.SEND_BUTTON_FONT_SIZE).Bold().AntiAliased()))
-        self.grid.Add(self.send_button, pos=(1, 3), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
+        self.grid.Add(self.send_button, pos=(0, 3), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
+
+        self.request_exchange_name_input = wx.TextCtrl(self)
+        self.request_exchange_name_input.SetHint("Request exchange name")
+        self.grid.Add(
+            self.request_exchange_name_input,
+            pos=(1, 0),
+            span=(0, 2),
+            flag=wx.EXPAND | wx.ALL,
+            border=DEFAULT_GAP,
+        )
+
+        self.request_routing_key_input = wx.TextCtrl(self)
+        self.request_routing_key_input.SetHint("Request routing key")
+        self.grid.Add(
+            self.request_routing_key_input,
+            pos=(1, 2),
+            span=(0, 2),
+            flag=wx.EXPAND | wx.ALL,
+            border=DEFAULT_GAP
+        )
+
+        self.response_queue_name_input = wx.TextCtrl(self)
+        self.response_queue_name_input.SetHint("Response queue name")
+        self.grid.Add(
+            self.response_queue_name_input,
+            pos=(2, 0),
+            span=(0, 1),
+            flag=wx.EXPAND | wx.ALL,
+            border=DEFAULT_GAP
+        )
+
+        self.response_exchange_name_input = wx.TextCtrl(self)
+        self.response_exchange_name_input.SetHint("Response exchange name")
+        self.grid.Add(
+            self.response_exchange_name_input,
+            pos=(2, 1),
+            span=(0, 1),
+            flag=wx.EXPAND | wx.ALL,
+            border=DEFAULT_GAP
+        )
+
+        self.response_exchange_name_input = wx.TextCtrl(self)
+        self.response_exchange_name_input.SetHint("Response routing key")
+        self.grid.Add(
+            self.response_exchange_name_input,
+            pos=(2, 2),
+            span=(0, 2),
+            flag=wx.EXPAND | wx.ALL,
+            border=DEFAULT_GAP
+        )
 
         self.request_data_notebook = RequestNotebook(self)
         self.grid.Add(
