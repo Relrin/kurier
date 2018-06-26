e4b7609b0f25e2645df1bb485210b617fa6f36bb
Valeryi Savich <relrin78@gmail.com>
2018-06-26 11:55:59 +0300

    Implemented extracting data from widgets and passing into AMQP request

--- kurier/widgets/request/data.py
+++ kurier/widgets/request/data.py
@@ -4,6 +4,7 @@ from kurier.widgets.text_ctrl import TextCtrl
 
 
 class RequestDataTab(wx.Panel):
+    TAB_SIZE = 4
 
     def __init__(self, *args, **kwargs):
         super(RequestDataTab, self).__init__(*args, **kwargs)
@@ -15,3 +16,13 @@ class RequestDataTab(wx.Panel):
     def InitUI(self):
         self.sizer.Add(self.message_body_ctrl, proportion=1, flag=wx.EXPAND)
         self.SetSizer(self.sizer)
+
+    def GetData(self):
+        text = self.message_body_ctrl.GetValue()
+        text = text.replace("\t", " " * self.TAB_SIZE)
+        text = text.replace("\n", "")
+
+        # Prevent replacing " and ' symbols onto duck-foot alternative on Mac OS X
+        text = text.replace("’", "\'")
+        text = text.replace("”", "\"")
+        return text
--- kurier/widgets/request/ui.py
+++ kurier/widgets/request/ui.py
@@ -3,12 +3,16 @@ import wx
 from wx.lib.scrolledpanel import ScrolledPanel
 
 from kurier.constants import DEFAULT_GAP,  DEFAULT_VERTICAL_GAP, DEFAULT_HORIZONTAL_GAP
+from kurier.amqp.rpc import RpcAmqpClient
 from kurier.widgets.request.notebook import RequestNotebook
 
 
 class RequestUIBlock(ScrolledPanel):
     SEND_BUTTON_FONT_SIZE = 10
     HORIZONTAL_SCROLL_INC = DEFAULT_GAP // 2 - 1
+
+    SEND_REQUEST_BUTTON_LABEL = "Send"
+    CANCEL_REQUEST_BUTTON_LABEL = "Cancel"
 
     def __init__(self, *args, **kwargs):
         super(RequestUIBlock, self).__init__(*args, **kwargs)
@@ -22,7 +26,10 @@ class RequestUIBlock(ScrolledPanel):
         self.send_button = None
         self.request_data_notebook = None
 
+        self.amqp_client = RpcAmqpClient(self)
+
         self.InitUI()
+        self.BindUI()
 
     def InitUI(self):
         self.connection_string = wx.TextCtrl(self)
@@ -36,9 +43,9 @@ class RequestUIBlock(ScrolledPanel):
         )
 
         button_font = wx.Font(wx.FontInfo(self.SEND_BUTTON_FONT_SIZE).Bold().AntiAliased())
-        self.send_button = wx.Button(self, label="Send", style=wx.BORDER_NONE)
+        self.send_button = wx.Button(self, label=self.SEND_REQUEST_BUTTON_LABEL, style=wx.BORDER_NONE)  # NOQA
         self.send_button.SetBackgroundColour("#20A5FF")
-        self.send_button.SetForegroundColour('#FFFFFF')
+        self.send_button.SetForegroundColour("#FFFFFF")
         self.send_button.SetFont(button_font)
         self.grid.Add(self.send_button, pos=(0, 3), flag=wx.EXPAND | wx.ALL, border=DEFAULT_GAP)
 
@@ -82,10 +89,10 @@ class RequestUIBlock(ScrolledPanel):
             border=DEFAULT_GAP
         )
 
-        self.response_exchange_name_input = wx.TextCtrl(self)
-        self.response_exchange_name_input.SetHint("Response routing key")
+        self.response_routing_key_input = wx.TextCtrl(self)
+        self.response_routing_key_input.SetHint("Response routing key")
         self.grid.Add(
-            self.response_exchange_name_input,
+            self.response_routing_key_input,
             pos=(2, 2),
             span=(0, 2),
             flag=wx.EXPAND | wx.ALL,
@@ -110,3 +117,28 @@ class RequestUIBlock(ScrolledPanel):
         self.static_box_sizer.Add(self.grid, proportion=1, flag=wx.EXPAND | wx.ALL)
         self.SetSizer(self.static_box_sizer)
         self.SetupScrolling(scroll_y=False, rate_x=self.HORIZONTAL_SCROLL_INC)
+
+    def BindUI(self):
+        self.Bind(wx.EVT_BUTTON, self.OnSendButtonClick)
+
+    def OnSendButtonClick(self, _event):
+        if self.send_button.GetLabel() == self.SEND_REQUEST_BUTTON_LABEL:
+            self.send_button.SetLabel(self.CANCEL_REQUEST_BUTTON_LABEL)
+            request_parameters = self.GetRequestParameters()
+            self.amqp_client.SendRequest(**request_parameters)
+        else:
+            self.amqp_client.CancelRequest()
+            self.send_button.SetLabel(self.SEND_REQUEST_BUTTON_LABEL)
+
+    def GetRequestParameters(self):
+        return {
+            "connection_url": self.connection_string.GetValue(),
+            "request_exchange": self.request_exchange_name_input.GetValue(),
+            "request_routing_key": self.request_routing_key_input.GetValue(),
+            "response_queue": self.response_queue_name_input.GetValue(),
+            "response_exchange": self.response_exchange_name_input.GetValue(),
+            "response_routing_key": self.response_routing_key_input.GetValue(),
+            "body": self.request_data_notebook.GetRequestData(),
+            "properties": self.request_data_notebook.GetRequestProperties(),
+            "headers": self.request_data_notebook.GetRequestHeaders()
+        }
