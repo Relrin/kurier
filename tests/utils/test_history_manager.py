2c7c8a52ab5c3d9f1f75add9b982d8673b714bc4
Valeryi Savich <relrin78@gmail.com>
2018-07-08 00:53:13 +0300

    Implemented tests for saving/loading request history

--- tests/utils/test_history_manager.py
+++ tests/utils/test_history_manager.py
@@ -1,3 +1,6 @@
+import json
+import tempfile
+
 import pytest
 
 from faker import Faker
@@ -6,6 +9,10 @@ from kurier.utils.history_manager import HistoryManager
 
 
 fake = Faker()
+TEMPFILE_OPTIONS = {
+    "newline": "\n",
+    "encoding": "utf-8"
+}
 
 
 def generate_state_data():
@@ -22,7 +29,7 @@ def generate_state_data():
         "response_exchange": "amqp.direct",
         "response_routing_key": fake.word(),
         "properties": {},
-        "headers": fake.pydict(),
+        "headers": {},
         "body": fake.sentence(),
     }
 
@@ -100,3 +107,83 @@ def test_removing_old_state_wont_be_applied_when_history_manager_is_empty():
 
     manager.RemoveOldState()
     assert manager.SavedStates == 0
+
+
+def test_manager_will_import_data_from_a_file():
+    with tempfile.NamedTemporaryFile(mode="r+", **TEMPFILE_OPTIONS) as import_file:
+        for _ in range(3):
+            state = generate_state_data()
+            state_dump = json.dumps(state) + TEMPFILE_OPTIONS["newline"]
+            import_file.write(state_dump)
+
+        import_file.seek(0)
+        manager = HistoryManager()
+        manager.ImportHistoryFromFile(import_file)
+
+        assert manager.SavedStates == 3
+
+
+def test_manager_wont_import_data_from_the_closed_file():
+    with tempfile.NamedTemporaryFile(mode="r+", **TEMPFILE_OPTIONS) as import_file:
+        import_file.close()
+        manager = HistoryManager()
+        manager.ImportHistoryFromFile(import_file)
+
+        assert manager.SavedStates == 0
+
+
+def test_manager_will_ignore_invalid_data_during_import_from_a_file():
+    with tempfile.NamedTemporaryFile(mode="r+", **TEMPFILE_OPTIONS) as import_file:
+        for _ in range(3):
+            state = generate_state_data()
+            state_dump = json.dumps(state) + TEMPFILE_OPTIONS["newline"]
+            import_file.write(state_dump)
+
+        import_file.write('{"KEY": "INVALID DATA"')
+
+        import_file.seek(0)
+        manager = HistoryManager()
+        manager.ImportHistoryFromFile(import_file)
+
+        assert manager.SavedStates == 3
+
+
+def test_manager_will_export_data_into_the_file():
+    with tempfile.NamedTemporaryFile(mode="r+", **TEMPFILE_OPTIONS) as import_file:
+        with tempfile.NamedTemporaryFile(mode="w+", **TEMPFILE_OPTIONS, delete=False) as export_file:  # NOQA
+
+            for _ in range(3):
+                state = generate_state_data()
+                state_dump = json.dumps(state) + TEMPFILE_OPTIONS["newline"]
+                import_file.write(state_dump)
+
+            import_file.seek(0)
+            manager = HistoryManager()
+            manager.ImportHistoryFromFile(import_file)
+
+            assert manager.SavedStates == 3
+
+            manager.ExportHistoryToFile(export_file)
+            export_file.close()
+
+            with open(export_file.name) as result_file:
+                assert len(result_file.read().splitlines()) == 3
+
+            export_file._closer.delete = True
+            export_file._closer.close()
+
+
+def test_manager_will_export_data_into_the_default_file():
+    with tempfile.TemporaryDirectory() as app_directory:
+        manager = HistoryManager(app_directory)
+
+        for _ in range(3):
+            state_data = generate_state_data()
+            manager.AddState(state_data)
+
+        assert manager.SavedStates == 3
+
+        manager.ExportHistoryToDefaultFile()
+
+        with open(manager._history_filepath) as result_file:
+            assert len(result_file.read().splitlines()) == 3
